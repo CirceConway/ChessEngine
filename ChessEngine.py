78afb14adf8ffea948db1538a4042d0cb7683c97
Circe Conway <circeconway@gmail.com>
2023-04-12 14:11:58 -0600

    First Commit to Push

---
+++ ChessEngine.py
@@ -0,0 +1,59 @@
+import chess
+#import chess.pgn
+from train_model import import_and_process, convert_fen, train_from_processed
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import numpy as np
+
+
+#1st dimension order is K, Q, R, B, N, P, k, q, r, b, n, p, en_passant,
+#special(Active player(a1), White Can Castle Kingside(e1), White can Castle Queenside(d1), Black Can Castle Kingside(e8), Black Can Castle Queenside(d8)) 
+layer_list = ['K', 'Q', 'R', 'B', 'N', 'P', 'k', 'q', 'r', 'b', 'n', 'p', 'en_passant', 'special']
+
+def choose_move(fen, model):
+    board = chess.Board(fen=fen)
+    converted_boards = []
+    for move in board.legal_moves:
+        board.push(move)
+        move_and_board = (move, convert_fen(board.fen()))
+        converted_board.append(move_and_board)
+        board.pop()
+    best_move = ''
+    best_score = 0
+    turn_mod = 1
+    if board.turn == chess.BLACK:
+        turn_mod = -1
+    for board in converted_boards:
+        pass
+
+
+def build_model(path):
+    model = models.Sequential()
+    #model.add(layers.Conv2D(1024, (1, 1), activation='relu', input_shape=(8, 8, 14)))
+    
+    model.add(layers.Dense(2048, activation='relu', input_shape = [768], batch_size=1))
+    model.add(layers.Dense(1024, activation='relu'))
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(1))
+
+    model.load_weights(path)
+
+    model.summary()
+
+    return model
+
+def evaluate(fen, model):
+    board = convert_fen(fen)
+    board = np.reshape(board, (1, 768))
+    prediction = model.predict(board, verbose=0)
+    #max_pre = np.argmax(prediction)
+    return prediction
+
+
+#import_and_process("GameData.pgn", "labeled_games_continuous.csv", 2000, skip_games=8000)
+
+#train_from_processed("labeled_games_continuous.csv", num_epochs=10)
+model = build_model("TenThousandGames.h5")
+
+print(evaluate("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", model))
+
---
+++ train_model.py
@@ -0,0 +1,125 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import chess.pgn
+import chess
+import csv
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+#model.summary()
+layer_list = ['K', 'Q', 'R', 'B', 'N', 'P', 'k', 'q', 'r', 'b', 'n', 'p']
+              #, 'en_passant', 'special']
+
+#Creates a board fill with "My pieces" and "Their pieces" instead of white and black
+def convert_fen(fen):
+    converted_board = np.zeros((8,8,12))
+    piece_split = fen.split()
+    board = piece_split[0].split("/")
+    turn_mod = 0
+    if piece_split[1] == 'b':
+        turn_mod = 1
+    for rank in range(len(board)):
+        board_file = 0
+        for file in range(len(board[rank])):
+            piece = board[rank][file]
+            if piece.isnumeric():
+                board_file += int(piece)
+            else:
+                layer_ind = layer_list.index(piece)
+                layer_ind = (layer_ind + (6*turn_mod)) % 12
+                converted_board[rank][board_file][layer_ind] = 1
+                board_file += 1
+
+    #Used for player turn and castling ability
+    #if piece_split[1] == 'b':
+    #    converted_board[0][0][13] = 1
+    #if 'K' in piece_split[2]:
+    #    converted_board[0][4][13] = 1
+    #if 'Q' in piece_split[2]:
+    #    converted_board[0][3][13] = 1
+    #if 'k' in piece_split[2]:
+    #    converted_board[7][4][13] = 1
+    #if 'q' in piece_split[2]:
+    #    converted_board[7][3][13] = 1
+
+    #Implement En passant eventually in layer index 12
+    #if piece_split[2] != '-':
+
+
+    return converted_board.flatten()
+
+#Extracts midgame and endgame positions from each game and stores them in out_path.
+#OUTPATH IS WIPED AT THE BEGINNING OF THIS FUNCTION
+def import_and_process(in_path, out_path, num_games, skip_games=0):
+    gamelist = open(in_path)
+    for i in range(skip_games):
+        chess.pgn.skip_game(gamelist)
+    with open(out_path, 'a') as file:
+        print("Beginning Processing")
+        csv_writer = csv.writer(file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        for i in range(num_games):
+            if i % 100 == 0:
+                print(f"Progress: {100 * (i/num_games)}%")
+            game = chess.pgn.read_game(gamelist)
+            label = game.headers["Result"].split('-')[0]
+            if label == '1/2':
+                label = 0.5
+            label = float(label)*2 - 1
+            #temp_board = chess.Board()
+            game = chess.pgn.read_game(gamelist)
+            move = 1
+            while game.next() is not None:
+                if move == 13 or move == 14:
+                    csv_writer.writerow([label, game.board().fen()])
+                if move % 29 == 0:
+                    csv_writer.writerow([label, game.board().fen()])
+                move += 1
+                game = game.next()
+            csv_writer.writerow([label, game.board().fen()])
+    #print(game)
+    #print(game.next().board().fen())
+                
+    #print(temp_game.variations)
+
+
+def train_from_processed(path, num_epochs=5):
+    print("Building Dataset...")
+    data = np.genfromtxt(path, delimiter = ',', dtype=str)
+
+    labels = []
+    boards = []
+    
+    for row in data:
+        labels.append(float(row[0]))
+        boards.append(convert_fen(row[1]))
+       
+    train_data, test_data, train_labels, test_labels = train_test_split(np.array(boards), np.array(labels), test_size=0.20, shuffle=True)
+
+    print("Beginning Training")
+    model = models.Sequential()
+    #model.add(layers.Conv2D(1024, (8, 8), activation='relu', input_shape=(8, 8, 14)))
+    
+    #model.add(layers.Flatten())
+
+    model.add(layers.Dense(2048, activation='relu', input_shape = [768]))
+    model.add(layers.Dense(1024, activation='relu'))
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(1))
+
+    model.load_weights("SimpleDenseMore.h5")
+
+    #model.summary()
+
+    model.compile(optimizer='adam',
+              loss=tf.keras.losses.mae,
+              metrics=['mae'])
+
+    history = model.fit(train_data, train_labels, epochs=num_epochs, 
+                    validation_data=(test_data, test_labels))
+
+    test_loss, test_acc = model.evaluate(test_data,  test_labels, verbose=2)
+
+    print(test_acc)
+
+    model.save_weights("TenThousandGames.h5")
+
